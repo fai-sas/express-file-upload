9c3b176a2e60767a856c539387e24ccdeb0cf870
fai-sas <craftedbyfaisal@gmail.com>
2024-02-02 18:33:43 +0600

    multiple files uploads added

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -4,14 +4,48 @@ const CustomError = require('../errors')
 const cloudinary = require('cloudinary').v2
 const fs = require('fs')
 
-const uploadProductImage = async (req, res) => {
-  const result = await cloudinary.uploader.upload(
-    req.files.image.tempFilePath,
-    { use_filename: true, folder: 'file-upload' }
-  )
-  fs.unlinkSync(req.files.image.tempFilePath)
+// const uploadProductImage = async (req, res) => {
+//   const result = await cloudinary.uploader.upload(
+//     req.files.image.tempFilePath,
+//     { use_filename: true, folder: 'file-upload' }
+//   )
+//   fs.unlinkSync(req.files.image.tempFilePath)
 
-  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } })
+//   return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } })
+// }
+
+const uploadProductImage = async (req, res) => {
+  try {
+    if (!req.files || (!Array.isArray(req.files.image) && !req.files.image)) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: 'Images are missing' })
+    }
+
+    let imageFiles = Array.isArray(req.files.image)
+      ? req.files.image
+      : [req.files.image]
+
+    const imagePromises = imageFiles.map(async (imageFile) => {
+      const result = await cloudinary.uploader.upload(imageFile.tempFilePath, {
+        use_filename: true,
+        folder: 'file-upload',
+      })
+
+      fs.unlinkSync(imageFile.tempFilePath)
+
+      return { src: result.secure_url }
+    })
+
+    const images = await Promise.all(imagePromises)
+
+    return res.status(StatusCodes.OK).json({ images })
+  } catch (error) {
+    console.error(error)
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: 'Internal Server Error' })
+  }
 }
 
 // const uploadProductImageLocal = async (req, res) => {
